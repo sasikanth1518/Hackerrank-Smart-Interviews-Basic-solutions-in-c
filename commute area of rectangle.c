d0b1699f3442fa8f0723e4cc45cf366b1465efde
Sasikanth Reddy Bhimireddy <131988411+sasikanth1518@users.noreply.github.com>
2023-05-17 20:58:44 +0530

    Create commute area of rectangle.c

---
+++ commute area of rectangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int l;
+    long b;
+    scanf("%d %lld",&l,&b);
+    printf("%lld",l*b);
+    return 0;
+}
